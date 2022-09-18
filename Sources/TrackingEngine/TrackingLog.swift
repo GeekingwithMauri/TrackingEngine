4c544abc767708432568681c1e126432dbe95426
Mauricio Chirino <1657723+mchirino89@users.noreply.github.com>
2022-09-18 08:47:57 -0300

    Update test doubles

--- Sources/TrackingEngine/TrackingLog.swift
+++ Sources/TrackingEngine/TrackingLog.swift
@@ -14,9 +14,19 @@ protocol TrackingLoggable {
     ///   - eventName: event name to be tracked. I.e. _view_appeared_, _button_tapped_ and so forth
     ///   - parameters: The dictionary of event parameters. Passing `nil` indicates that the event has no parameters.
     func track(eventName: String, parameters: [String: Any]?)
+
+    /// Tracks errors alongside its context on the cloud provider. For the time being, that is **Firebase**
+    /// - Parameters:
+    ///   - errorName: error name to be tracked.
+    ///   - parameters: The dictionary of metadata context.
+    func log(errorName: String, parameters: [String: Any])
 }
 
 struct TrackingLog: TrackingLoggable {
+    func log(errorName: String, parameters: [String : Any]) {
+        
+    }
+
     func track(eventName: String, parameters: [String : Any]?) {
         Analytics.logEvent(eventName, parameters: parameters)
     }
